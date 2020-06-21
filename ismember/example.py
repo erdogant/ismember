970096cf2fec29d46033566216c052e941553e44
Erdogan Taskesen <erdogant@gmail.com>
2020-06-22 00:24:23 +0200

    speed check added in examples

--- ismember/example.py
+++ ismember/example.py
@@ -47,3 +47,37 @@ Iloc, idx = ismember(a_vec, b_vec, 'rows')
 i=0
 a_vec[i,Iloc[i]]==b_vec[i,idx[i]]
 
+# %%
+from datetime import datetime
+
+t1=[]
+t2=[]
+ns = np.random.randint(10,10000,1000)
+
+for n in ns:
+    a_vec = np.random.randint(0,100,n)
+    b_vec = np.random.randint(0,100,n)
+    # Run stack version
+    start = datetime.now()
+    out1=ismember_stack(a_vec, b_vec)
+    end = datetime.now()
+    t1.append(end - start)
+
+    # Run ismember
+    start = datetime.now()
+    out2=ismember(a_vec, b_vec)
+    end = datetime.now()
+    t2.append(end - start)
+
+#
+print(np.sum(t1))
+# ismember pypi
+print(np.sum(t2))
+
+# %%
+def ismember_stack(a, b):
+    bind = {}
+    for i, elt in enumerate(b):
+        if elt not in bind:
+            bind[elt] = i
+    return [bind.get(itm, None) for itm in a]  # None can be replaced by any other "not in b" value
