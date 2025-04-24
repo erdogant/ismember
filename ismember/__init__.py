b2f0f5243eb1c87f30bc14e01dfb532bb70e3dfa
erdogant <erdogant@gmail.com>
2025-04-24 14:07:03 +0200

    new version and cleaning old files

--- ismember/__init__.py
+++ ismember/__init__.py
@@ -2,7 +2,7 @@ from ismember.ismember import ismember, is_row_in
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 # module level doc-string
 __doc__ = """
