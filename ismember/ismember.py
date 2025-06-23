546f41efcfbfc518eb24b41a5c02e501188c4be6
erdogant <erdogant@gmail.com>
2025-06-23 15:54:13 +0200

    code refactoring

--- ismember/ismember.py
+++ ismember/ismember.py
@@ -141,8 +141,8 @@ def _settypes(a_vec, b_vec):
 def _compute(a_vec, b_vec):
     bool_ind = np.isin(a_vec, b_vec)
     common = a_vec[bool_ind]
-    [common_unique, common_inv] = np.unique(common, return_inverse=True)
-    [b_unique, b_ind] = np.unique(b_vec, return_index=True)
+    common_unique, common_inv = np.unique(common, return_inverse=True)
+    b_unique, b_ind = np.unique(b_vec, return_index=True)
     common_ind = b_ind[np.isin(b_unique, common_unique, assume_unique=True)]
     return bool_ind, common_ind[common_inv]
 
